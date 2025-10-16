008f4117541f274a3b63d2c5323e8b8d12dca8fd
AntonioGaoEstaCogido <zgao01@ucm.es>
2025-10-16 20:55:07 +0200

    Botón de salir del menú

--- scrabble-definitivo/Opciones/opciones.gd
+++ scrabble-definitivo/Opciones/opciones.gd
@@ -1,10 +1,11 @@
 extends Control
 
-func salir():
-	get_tree().paused = false
-
 const TUTORIAL_SCENE := preload("res://Opciones/Tutorial/Tutorial.tscn")
 
 func _on_tutorial_pressed() -> void:
 	var t = TUTORIAL_SCENE.instantiate()
 	get_tree().current_scene.add_child(t)
+
+
+func _on_salir_pressed() -> void:
+		get_tree().paused = false
