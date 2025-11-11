2528879609fa08e9b89f6d614f7cbb26a6f1cc8d
AntonioGaoEstaCogido <zgao01@ucm.es>
2025-11-11 21:07:04 +0100

    Volver al menú inicial desde opciones

--- scrabble-definitivo/Opciones/Tutorial/opciones.gd
+++ scrabble-definitivo/Opciones/Tutorial/opciones.gd
@@ -1,6 +1,7 @@
 extends Control
 
 const TUTORIAL_SCENE := preload("res://Opciones/Tutorial/Tutorial.tscn")
+const MENU_SCENE := preload("res://Opciones/Menú principal/Menú principal.tscn")
 
 func _on_tutorial_pressed() -> void:
 	var t = TUTORIAL_SCENE.instantiate()
@@ -8,3 +9,7 @@ func _on_tutorial_pressed() -> void:
 
 func _on_salir_pressed() -> void:
 		queue_free()
+
+func _on_button_4_pressed() -> void:
+	var t = MENU_SCENE.instantiate()
+	get_tree().current_scene.add_child(t)
