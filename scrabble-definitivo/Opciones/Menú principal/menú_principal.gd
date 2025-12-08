6d08045dccbe782154686b5342258f41957e821e
AntonioGaoEstaCogido <zgao01@ucm.es>
2025-12-08 03:48:41 +0100

    se me habia olvidado lo de las donaciones, perdon

--- scrabble-definitivo/Opciones/Menú principal/menú_principal.gd
+++ scrabble-definitivo/Opciones/Menú principal/menú_principal.gd
@@ -10,6 +10,7 @@ const CONTROLES_SCENE := preload ("res://Opciones/Tutorial/controles.tscn")
 const GUARDAR_SCENE := preload ("res://Online/pantalla_guardado.tscn")
 const MISIONES_SCENE := preload ("res://Escena_misiones/Misiones.tscn")
 const TIENDA_SCENE := preload ("res://Tienda/Tienda.tscn")
+const DONAR_SCENE := preload ("res://Opciones/donación.tscn")
 
 func show_error_message():
 	error.text = "Inicia sesión para poder acceder a esto"
@@ -76,3 +77,9 @@ func _on_tienda_pressed() -> void:
 		var t = TIENDA_SCENE.instantiate()
 		get_tree().current_scene.add_child(t)
 	show_error_message()
+
+
+func _on_donacion_pressed() -> void:
+	$SFXPlayer.play()
+	var t = DONAR_SCENE.instantiate()
+	get_tree().current_scene.add_child(t)
